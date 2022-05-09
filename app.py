7c99e32fe88d6c15c00633df7f96f8bbe73a4540
rajeshai <73292655+rajeshai@users.noreply.github.com>
2022-05-09 13:38:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,13 @@ def main():
     model.fit(train_images, train_labels, epochs=10, 
                     validation_data=(test_images, test_labels))
 
-    model.save('my_model.h5')
+    filepath = './saved_model'
+    tf.keras.models.save_model(
+    model,
+    filepath)
+
+
+    #model.save('my_model.h5')
 
 if __name__ == '__main__':
     main()
